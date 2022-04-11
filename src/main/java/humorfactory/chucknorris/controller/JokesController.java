292522edd05bf12fafa1a118d958fe35d26b401e
mdomanowski <83656137+mdomanowski@users.noreply.github.com>
2022-04-11 20:17:10 +0200

    Added not secure link

--- src/main/java/humorfactory/chucknorris/controller/JokesController.java
+++ src/main/java/humorfactory/chucknorris/controller/JokesController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class JokesController {
     private final JokesService jokesService = new JokesService();
-    @CrossOrigin(origins = {"http://localhost:4200/", "https://templeofchuck.surge.sh/"})
+    @CrossOrigin(origins = {"http://localhost:4200/", "https://templeofchuck.surge.sh/", "http://templeofchuck.surge.sh/"})
     @GetMapping("/joke")
     public ResponseEntity<JokeDTO> getJoke() {
         return ResponseEntity.ok(jokesService.getRandomJoke());
